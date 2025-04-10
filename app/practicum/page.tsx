d41e876ee005b5cbbcca81ce11911e608853e545
adnan <adnanhassanda@gmail.com>
2025-04-10 08:48:23 +0800

    add placeholder to gallery

--- app/practicum/page.tsx
+++ app/practicum/page.tsx
@@ -3,6 +3,7 @@ import Footer from "../components/ui/footer"
 import Company from "../components/layout/company";
 import VidResume from "../components/layout/vidResume";
 import Documents from "../components/layout/documents";
+import Gallery from "../components/layout/gallery";
 
 export default function Practicum() {
     return (
@@ -11,6 +12,7 @@ export default function Practicum() {
             <Company/>
             <VidResume/>
             <Documents/>
+            <Gallery/>
             <Footer/>
         </main>
     ); 
