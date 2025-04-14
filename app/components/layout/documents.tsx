e098acc0b09d59809f5f269d40e9a5fd688263f5
adnan <adnanhassanda@gmail.com>
2025-04-14 17:21:43 +0800

    add tracking documents

--- app/components/layout/documents.tsx
+++ app/components/layout/documents.tsx
@@ -47,21 +47,24 @@ export default function Documents() {
     ]
 
     return (
-        <div className="flex w-full flex-col lg:flex-row p-10 flex-wrap gap-4 justify-center">
-            {documents.map((doc) => (
-                <div key={doc.id} className="card bg-base-300 rounded-box p-4 w-full lg:w-1/3">
-                    <h2 className="text-lg font-bold">{doc.title}</h2>
-                    <p className="text-sm">{doc.description}</p>
-                    <a
-                        href={doc.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-blue-500 underline mt-2 block"
-                    >
-                        View Document
-                    </a>
-                </div>
-            ))}
+        <div>
+            <h1 className="font-barlow font-bold text-5xl sm:px-8 md:px-16 lg:px-24 pt-10 flex justify-center">DOCUMENTS</h1>
+            <div className="flex w-full flex-col lg:flex-row p-10 flex-wrap gap-4 justify-center">
+                {documents.map((doc) => (
+                    <div key={doc.id} className="card bg-base-300 rounded-box p-4 w-full lg:w-1/3">
+                        <h2 className="text-lg font-bold">{doc.title}</h2>
+                        <p className="text-sm">{doc.description}</p>
+                        <a
+                            href={doc.link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-[#F97316] underline flex justify-end mt-2 block"
+                        >
+                            View Document
+                        </a>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
--- app/components/layout/tracking.tsx
+++ app/components/layout/tracking.tsx
@@ -21,21 +21,24 @@ export default function Tracking() {
     ]
 
     return (
-        <div className="flex w-full flex-col lg:flex-row p-10 flex-wrap gap-4 justify-center">
-            {trackingDocuments.map((doc) => (
-                <div key={doc.id} className="card bg-base-300 rounded-box p-4 w-full lg:w-1/3">
-                    <h2 className="text-lg font-bold">{doc.title}</h2>
-                    <p className="text-sm">{doc.description}</p>
-                    <a
-                        href={doc.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-blue-500 underline mt-2 block"
-                    >
-                        View Document
-                    </a>
-                </div>
-            ))}
+        <div>
+            <h1 className="font-barlow text-5xl font-black sm:px-8 md:px-16 lg:px-24 flex justify-center">TRACKING</h1>
+            <div className="flex w-full flex-col lg:flex-row p-10 flex-wrap gap-4 justify-center">
+                {trackingDocuments.map((doc) => (
+                    <div key={doc.id} className="card bg-base-300 rounded-box p-4 w-full lg:w-1/3">
+                        <h2 className="text-lg font-bold">{doc.title}</h2>
+                        <p className="text-sm">{doc.description}</p>
+                        <a
+                            href={doc.link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-[#F97316] underline flex justify-end mt-2 block"
+                        >
+                            View Document
+                        </a>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
