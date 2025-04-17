14929950528055ae38fe839a3d1ee573f2295a3e
Adnan <adnanhassanda@gmail.com>
2025-04-17 23:37:11 +0800

    fix gallery

--- app/components/layout/gallery.tsx
+++ app/components/layout/gallery.tsx
@@ -4,27 +4,23 @@ export default function Gallery() {
     return (
         <div className="w-full overflow-x-auto scrollbar-visible">
             <div className="carousel carousel-center rounded-box relative w-max h-96 mt-10 mb-2 px-2 flex gap-2">
-                <div className="carousel-item">
-                    <img src="/gallery1.png" alt="Burger" />
-                </div>
-                <div className="carousel-item">
-                    <img src="/gallery2.png" alt="Burger" />
-                </div>
-                <div className="carousel-item">
-                    <img src="/gallery3.png" alt="Burger" />
-                </div>
-                <div className="carousel-item">
-                    <img src="/gallery4.png" alt="Burger" />
-                </div>
-                <div className="carousel-item">
-                    <img src="/gallery5.png" alt="Burger" />
-                </div>
-                <div className="carousel-item">
-                    <img src="/gallery6.png" alt="Burger" />
-                </div>
-                <div className="carousel-item">
-                    <img src="/gallery7.jpg" alt="Burger" />
-                </div>
+                {[
+                    "/gallery1.png",
+                    "/gallery2.png",
+                    "/gallery3.png",
+                    "/gallery4.png",
+                    "/gallery5.png",
+                    "/gallery6.png",
+                    "/gallery7.jpg",
+                ].map((src, idx) => (
+                    <div key={idx} className="carousel-item">
+                        <img
+                            src={src}
+                            alt={`Gallery ${idx + 1}`}
+                            className="h-full w-auto object-contain"
+                        />
+                    </div>
+                ))}
             </div>
         </div>
     )
