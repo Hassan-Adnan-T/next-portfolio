1f1d51f6d5639d4c6be95ea33b85780e8d2b8361
adnan <adnanhassanda@gmail.com>
2025-04-17 20:09:21 +0800

    add placeholder for another video

--- app/practicum/page.tsx
+++ app/practicum/page.tsx
@@ -15,6 +15,7 @@ export default function Practicum() {
             <Documents/>
             <Tracking/>
             <Gallery/>
+            <VidResume/>
             <Footer/>
         </main>
     ); 
