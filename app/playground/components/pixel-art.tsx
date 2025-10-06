5aeadd9dddf3b656838878e7cee1175476673eeb
Nanexist <adnanhassanda@gmail.com>
2025-10-06 12:38:56 +0800

    add pixel art section

---
+++ app/playground/components/pixel-art.tsx
@@ -0,0 +1,9 @@
+export default function PixelArt() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-black text-4xl font-bold font-barlow">Pixel Art</h1>
+            <div className="text-black flex flex-col items-center justify-center font-karla">
+            </div>
+        </div>
+    )
+}
--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "../components/ui/navbar"
 import Footer from "../components/ui/footer"
 import PageTransition from "../components/ui/page-transition"
 import UIDesign from "./components/ui-design"
+import PixelArt from "./components/pixel-art"
 
 export default function Playground() {
     return (
@@ -9,6 +10,7 @@ export default function Playground() {
             <main className="relative bg-white">
                 <Navbar/>
                 <UIDesign/>
+                <PixelArt/>
                 <Footer/>
             </main>
         </PageTransition>
