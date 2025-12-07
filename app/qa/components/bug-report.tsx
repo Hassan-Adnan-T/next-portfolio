4672fbdfd01af0f3a36579f686be744233146df7
Nanexist <adnanhassanda@gmail.com>
2025-12-07 10:54:28 +0800

    add bug report

---
+++ app/qa/components/bug-report.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function BugReport() {
+
+  return (
+    <div className="p-50 min-h-screen bg-gray-50 p-8" >
+    </div>
+  );
+}
--- app/qa/page.tsx
+++ app/qa/page.tsx
@@ -2,13 +2,15 @@ import Navbar from "../components/ui/navbar"
 import Footer from "../components/ui/footer"
 import PageTransition from "../components/ui/page-transition"
 import Templates from "./components/templates"
+import BugReport from "./components/bug-report"
 
 export default function QA() {
     return (
         <PageTransition>
             <main className="relative bg-white">
                 <Navbar/>
-                <Templates/>    
+                <Templates/>   
+                <BugReport/> 
                 <Footer/>
             </main>
         </PageTransition>
